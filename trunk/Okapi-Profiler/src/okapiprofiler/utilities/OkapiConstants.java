d853c41436928a7becf28af95df6e2a06f2dc7c0
tomoodesign@gmail.com <tomoodesign@gmail.com@fbce5bb2-f101-e12c-8c55-5ded80b935fc>
2011-05-12 02:07:56 +0000

    update

---
+++ trunk/Okapi-Profiler/src/okapiprofiler/utilities/OkapiConstants.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package okapiprofiler.utilities;
+
+/**
+ *
+ * @author QiangWu
+ */
+public class OkapiConstants {
+    public static final String[] DEBUGTABLEHEADER = {"Variable", "Value"};
+    
+}
